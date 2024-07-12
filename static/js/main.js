b0cc41b2f448474a89f22d59f33093a397e59bef
shenc <stephenshenc@gmail.com>
2024-07-12 18:27:53 +0800

    weather deleted

--- static/js/main.js
+++ static/js/main.js
@@ -106,64 +106,6 @@ $('#hitokoto').click(function () {
     }
 });
 
-//获取天气
-//请前往 https://www.mxnzp.com/doc/list 申请 app_id 和 app_secret
-//请前往 https://dev.qweather.com/ 申请 key
-
-const add_id = "vcpmlmqiqnjpxwq1"; // app_id
-const app_secret = "PeYnsesgkmK7qREhIFppIcsoN0ZShv3c"; // app_secret
-const key = "691d007d585841c09e9b41e79853ecc2" // key
-function getWeather() {
-    fetch("https://www.mxnzp.com/api/ip/self?app_id=" + add_id + "&app_secret=" + app_secret)
-        .then(response => response.json())
-        .then(data => {
-            let str = data.data.city
-            let city = str.replace(/市/g, '')
-            console.log(data,"sssss")
-            $('#city_text').html(city);
-            fetch("https://geoapi.qweather.com/v2/city/lookup?location=" + city + "&number=1&key=" + key)
-                .then(response => response.json())
-                .then(location => {
-                    let id = location.location[0].id
-                    fetch("https://devapi.qweather.com/v7/weather/now?location=" + id + "&key=" + key)
-                        .then(response => response.json())
-                        .then(weather => {
-                            $('#wea_text').html(weather.now.text)
-                            $('#tem_text').html(weather.now.temp + "°C&nbsp;")
-                            $('#win_text').html(weather.now.windDir)
-                            $('#win_speed').html(weather.now.windScale + "级")
-                        })
-                })
-        })
-        .catch(console.error);
-}
-getWeather();
-
-let wea = 0;
-$('#upWeather').click(function () {
-    if (wea == 0) {
-        wea = 1;
-        let index = setInterval(function () {
-            wea--;
-            if (wea == 0) {
-                clearInterval(index);
-            }
-        }, 60000);
-        getWeather();
-        iziToast.show({
-            timeout: 2000,
-            icon: "fa-solid fa-cloud-sun",
-            message: '实时天气已更新'
-        });
-    } else {
-        iziToast.show({
-            timeout: 1000,
-            icon: "fa-solid fa-circle-exclamation",
-            message: '请稍后再更新哦'
-        });
-    }
-});
-
 //获取时间
 let t = null;
 t = setTimeout(time, 1000);
